e6b5de0e7dec866a1aec7a2a7396fcd366a0c97c
GuangxiangLong <GuangxiangLong@GuangxiangLong-pc>
2016-10-26 18:37:16 +0800

    生产工序记录 物料记录 服务层第一次上传

---
+++ softfactory/service/com/softfactory/core/service/MProcedureModulingService.java
@@ -0,0 +1,40 @@
+package com.softfactory.core.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.softfactory.core.dao.MProcedureModulingMapper;
+import com.softfactory.core.util.Pager;
+import com.softfactory.pojo.MProcedureModuling;
+
+@Service("mProcedureModulingService")
+public class MProcedureModulingService {
+
+	@Resource(name = "mProcedureModulingMapper")
+	private MProcedureModulingMapper mProcedureModulingMapper;
+
+	public Pager<MProcedureModuling> findPager(Integer page, Integer pagesize, String sort, String order) {
+		Pager<MProcedureModuling> p = new Pager<MProcedureModuling>();
+		p.setRows(mProcedureModulingMapper.findPager(page, pagesize, sort, order));
+		p.setTotal(mProcedureModulingMapper.findPagerTotal());
+		return p;
+	}
+
+	public int add(MProcedureModuling m) {
+		return mProcedureModulingMapper.add(m);
+	}
+
+	public int update(MProcedureModuling m) {
+		return mProcedureModulingMapper.update(m);
+	}
+
+	public int remove(Integer mPId) {
+		return mProcedureModulingMapper.remove(mPId);
+	}
+
+	public MProcedureModuling findById(Integer mPId) {
+		return mProcedureModulingMapper.findById(mPId);
+	}
+
+}
---
+++ softfactory/service/com/softfactory/core/service/MProceduringService.java
@@ -0,0 +1,74 @@
+package com.softfactory.core.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.softfactory.core.dao.MProceduringMapper;
+import com.softfactory.core.util.Pager;
+import com.softfactory.pojo.MProceduring;
+
+@Service("mProceduringService")
+public class MProceduringService {
+
+	@Resource(name = "mProceduringMapper")
+	private MProceduringMapper mProceduringMapper;
+
+	/**
+	 * 分页查询
+	 * 
+	 * @param page
+	 * @param pagesize
+	 * @param sort
+	 * @param order
+	 * @return
+	 */
+	public Pager<MProceduring> findPager(Integer page, Integer pagesize, String sort, String order) {
+		Pager<MProceduring> p = new Pager<MProceduring>();
+		p.setRows(mProceduringMapper.findPager(page, pagesize, sort, order));
+		p.setTotal(mProceduringMapper.findPagerTotal());
+		return p;
+	}
+
+	/**
+	 * 增加一条生产记录
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public int add(MProceduring m) {
+		return mProceduringMapper.add(m);
+	}
+
+	/**
+	 * 修改一条生产记录
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public int update(MProceduring m) {
+
+		return mProceduringMapper.update(m);
+	}
+
+	/**
+	 * 通过主键查询一条生产记录
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public MProceduring findById(Integer mPMId) {
+
+		return mProceduringMapper.findById(mPMId);
+	}
+
+	/**
+	 * 删除一条生产记录
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public int del(Integer mPMId) {
+		return mProceduringMapper.del(mPMId);
+	}
+}
