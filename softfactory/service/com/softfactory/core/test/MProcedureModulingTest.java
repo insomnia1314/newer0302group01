e8d9f1c4b807bd60b313628541ec372abf5b5c4a
GuangxiangLong <GuangxiangLong@GuangxiangLong-pc>
2016-10-26 18:39:27 +0800

    生产过程记录 物料记录 测试

---
+++ softfactory/service/com/softfactory/core/test/MProcedureModulingTest.java
@@ -0,0 +1,85 @@
+package com.softfactory.core.test;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.softfactory.core.service.MProcedureModulingService;
+import com.softfactory.core.util.Pager;
+import com.softfactory.pojo.MProcedureModuling;
+
+/**
+ * 龙光祥的产品物料测试
+ * 
+ * @author GuangxiangLong
+ *
+ */
+public class MProcedureModulingTest {
+
+	private MProcedureModulingService mProcedureModulingService;
+
+	@SuppressWarnings("resource")
+	@Before
+	public void init() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		mProcedureModulingService = (MProcedureModulingService) ctx.getBean("mProcedureModulingService");
+	}
+
+	@Test
+	public void testAdd() {
+		MProcedureModuling m = new MProcedureModuling();
+		m.setAmount(1);
+		m.setCostPrice(20);
+		m.setDetailsNumber(1);
+		m.setParentId(8);
+		m.setProducId("0001");
+		m.setProductName("三星Boom7");
+		m.setSubtotal(600);
+		mProcedureModulingService.add(m);
+	}
+
+	@Test
+	public void testSearch() {
+		MProcedureModuling m = mProcedureModulingService.findById(10);
+		System.out.println(m);
+	}
+
+	@Test
+	public void testPager() {
+
+		Integer page = 1;
+		Integer rows = 5;
+		String sort = "asc";
+		String order = "asc";
+		Integer pagesize = page * rows;
+		page = (page - 1) * rows;
+		Pager<MProcedureModuling> p = mProcedureModulingService.findPager(page, pagesize, sort, order);
+		List<MProcedureModuling> l = p.getRows();
+		System.out.println("总记录数字：" + p.getTotal());
+		for (MProcedureModuling m : l) {
+			System.out.println(m);
+		}
+	}
+
+	@Test
+	public void testUpdate() {
+		MProcedureModuling m = new MProcedureModuling();
+		m.setmPId(10);
+		m.setAmount(1);
+		m.setCostPrice(20);
+		m.setDetailsNumber(1);
+		m.setParentId(8);
+		m.setProducId("0001");
+		m.setProductName("三星Noot7");
+		m.setSubtotal(600);
+		mProcedureModulingService.update(m);
+	}
+
+	@Test
+	public void testDel() {
+		mProcedureModulingService.remove(9);
+	}
+}
---
+++ softfactory/service/com/softfactory/core/test/MProceduringTest.java
@@ -0,0 +1,92 @@
+package com.softfactory.core.test;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.softfactory.core.service.MProceduringService;
+import com.softfactory.core.util.Pager;
+import com.softfactory.pojo.MProceduring;
+
+public class MProceduringTest {
+	private MProceduringService mProceduringService;
+
+	@SuppressWarnings("resource")
+	@Before
+	public void init() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		mProceduringService = (MProceduringService) ctx.getBean("mProceduringService");
+	}
+
+	@Test
+	public void testAdd() {
+		MProceduring m = new MProceduring();
+		m.setChecker("Checker");
+		m.setCheckTime(new Date());
+		m.setCostPrice(200.0d);
+		m.setDetailsNumber(1);
+		m.setLabourHourAmount(11);
+		m.setParentId(1);
+		m.setProcedureDescribe("ProcedureDescribe");
+		m.setProcedureId("ProcedureId");
+		m.setProcedureName("ProcedureNam");
+		m.setProcedureResponsiblePerson("procedureResponsiblePerson");
+		m.setRegCount(11);
+		m.setRegister("register");
+		m.setRegisterTime(new Date());
+		m.setSubtotal(2);
+		mProceduringService.add(m);
+	}
+
+	@Test
+	public void testFindById() {
+		MProceduring m = mProceduringService.findById(13);
+		System.out.println(m);
+	}
+
+	@Test
+	public void testFindPager() {
+		Integer page = 1;
+		Integer rows = 5;
+		String sort = "asc";
+		String order = "asc";
+		Integer pagesize = page * rows;
+		page = (page - 1) * rows;
+		Pager<MProceduring> p = mProceduringService.findPager(page, pagesize, sort, order);
+		List<MProceduring> l = p.getRows();
+		System.out.println("总记录数字：" + p.getTotal());
+		for (MProceduring m : l) {
+			System.out.println(m);
+		}
+	}
+
+	@Test
+	public void testUpdate() {
+		MProceduring m = new MProceduring();
+		m.setmPMId(13);
+		m.setChecker("Checker");
+		m.setCheckTime(new Date());
+		m.setCostPrice(200.0d);
+		m.setDetailsNumber(1);
+		m.setLabourHourAmount(11);
+		m.setParentId(1);
+		m.setProcedureDescribe("ProcedureDescribe");
+		m.setProcedureId("ProcedureId");
+		m.setProcedureName("ProcedureNam");
+		m.setProcedureResponsiblePerson("procedureResponsiblePerson");
+		m.setRegCount(11);
+		m.setRegister("register");
+		m.setRegisterTime(new Date());
+		m.setSubtotal(2);
+		mProceduringService.update(m);
+	}
+
+	@Test
+	public void testDel() {
+		mProceduringService.del(8);
+	}
+}
