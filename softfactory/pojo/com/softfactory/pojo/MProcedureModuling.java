63ca46a6d2528936d16b24c147b769598847806f
GuangxiangLong <GuangxiangLong@GuangxiangLong-pc>
2016-10-26 18:43:42 +0800

    生产记录，生产物料 POJO第二次跟新上传

--- softfactory/pojo/com/softfactory/pojo/MProcedureModuling.java
+++ softfactory/pojo/com/softfactory/pojo/MProcedureModuling.java
@@ -1,74 +1,94 @@
 package com.softfactory.pojo;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * 生产工序物料过程记录
+ * 
  * @author GuangxiangLong
  *
  */
+
 public class MProcedureModuling implements Serializable {
-	
+
 	private static final long serialVersionUID = 3726082431537144304L;
 
-	private Integer mPId; //主键序号 
-	private Integer parentId;  //派工单工序序号
-	private Integer detailsNumber; //本工序物料序号
-	private String producId;    //产品编号
-	private String productName; //产品名称
-	private double costPrice;   //单位物料成本
-	private double amount; 	    //本次使用数量
-	private double subtotal;    //物料成本
-	
+	private Integer mPId; // 主键序号
+	private Integer parentId; // 派工单工序序号
+	private Integer detailsNumber; // 本工序物料序号
+	private String producId; // 产品编号
+	private String productName; // 产品名称
+	private double costPrice; // 单位物料成本
+	private double amount; // 本次使用数量
+	private double subtotal; // 物料成本
+
 	public Integer getmPId() {
 		return mPId;
 	}
+
 	public void setmPId(Integer mPId) {
 		this.mPId = mPId;
 	}
+
 	public Integer getParentId() {
 		return parentId;
 	}
+
 	public void setParentId(Integer parentId) {
 		this.parentId = parentId;
 	}
+
 	public Integer getDetailsNumber() {
 		return detailsNumber;
 	}
+
 	public void setDetailsNumber(Integer detailsNumber) {
 		this.detailsNumber = detailsNumber;
 	}
+
 	public String getProducId() {
 		return producId;
 	}
+
 	public void setProducId(String producId) {
 		this.producId = producId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public double getCostPrice() {
 		return costPrice;
 	}
+
 	public void setCostPrice(double costPrice) {
 		this.costPrice = costPrice;
 	}
+
 	public double getAmount() {
 		return amount;
 	}
+
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
+
 	public double getSubtotal() {
 		return subtotal;
 	}
+
 	public void setSubtotal(double subtotal) {
 		this.subtotal = subtotal;
 	}
-	
-	
+
+	public String toString() {
+		return "MProcedureModuling [mPId=" + mPId + ", parentId=" + parentId + ", detailsNumber=" + detailsNumber
+				+ ", producId=" + producId + ", productName=" + productName + ", costPrice=" + costPrice + ", amount="
+				+ amount + ", subtotal=" + subtotal + "]";
+	}
 }
--- softfactory/pojo/com/softfactory/pojo/MProceduring.java
+++ softfactory/pojo/com/softfactory/pojo/MProceduring.java
@@ -3,120 +3,164 @@ package com.softfactory.pojo;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 /**
  * 生产工序物料过程记录
+ * 
  * @author GuangxiangLong
  *
  */
-public class MProceduring implements Serializable{
-	
+
+public class MProceduring implements Serializable {
+
 	private static final long serialVersionUID = -5900185808975822112L;
-	
+
 	private Integer mPMId; // 主键序号
-	private Integer parentId;  //派工单序号
-	private Integer detailsNumber; //工序序号
-	private String procedureId; //工序编号
-	private String ProcedureName; //工序名称
-	private double labourHourAmount; //本次工时数
-	private double costPrice; //单位工时成本
-	private double subtotal; //工时成本小计
-	private String procedureDescribe; //工序描述
-	private double regCount; //登记次数
-	private String procedureResponsiblePerson;//负责人
-	private String register;//登记人
-	private Date registerTime;//登记时间
-	private String checker;//审核人
-	private String checkTime;//审核时间
-	
+	private Integer parentId; // 派工单序号
+	private Integer detailsNumber; // 工序序号
+	private String procedureId; // 工序编号
+	private String procedureName; // 工序名称
+	private double labourHourAmount; // 本次工时数
+	private double costPrice; // 单位工时成本
+	private double subtotal; // 工时成本小计
+	private String procedureDescribe; // 工序描述
+	private double regCount; // 登记次数
+	private String procedureResponsiblePerson;// 负责人
+	private String register;// 登记人
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date registerTime;// 登记时间
+	private String checker;// 审核人
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date checkTime;// 审核时间
+
 	public Integer getmPMId() {
 		return mPMId;
 	}
+
 	public void setmPMId(Integer mPMId) {
 		this.mPMId = mPMId;
 	}
+
 	public Integer getParentId() {
 		return parentId;
 	}
+
 	public void setParentId(Integer parentId) {
 		this.parentId = parentId;
 	}
+
 	public Integer getDetailsNumber() {
 		return detailsNumber;
 	}
+
 	public void setDetailsNumber(Integer detailsNumber) {
 		this.detailsNumber = detailsNumber;
 	}
+
 	public String getProcedureId() {
 		return procedureId;
 	}
+
 	public void setProcedureId(String procedureId) {
 		this.procedureId = procedureId;
 	}
+
 	public String getProcedureName() {
-		return ProcedureName;
+		return procedureName;
 	}
+
 	public void setProcedureName(String procedureName) {
-		ProcedureName = procedureName;
+		this.procedureName = procedureName;
 	}
+
 	public double getLabourHourAmount() {
 		return labourHourAmount;
 	}
+
 	public void setLabourHourAmount(double labourHourAmount) {
 		this.labourHourAmount = labourHourAmount;
 	}
+
 	public double getCostPrice() {
 		return costPrice;
 	}
+
 	public void setCostPrice(double costPrice) {
 		this.costPrice = costPrice;
 	}
+
 	public double getSubtotal() {
 		return subtotal;
 	}
+
 	public void setSubtotal(double subtotal) {
 		this.subtotal = subtotal;
 	}
+
 	public String getProcedureDescribe() {
 		return procedureDescribe;
 	}
+
 	public void setProcedureDescribe(String procedureDescribe) {
 		this.procedureDescribe = procedureDescribe;
 	}
+
 	public double getRegCount() {
 		return regCount;
 	}
+
 	public void setRegCount(double regCount) {
 		this.regCount = regCount;
 	}
+
 	public String getProcedureResponsiblePerson() {
 		return procedureResponsiblePerson;
 	}
+
 	public void setProcedureResponsiblePerson(String procedureResponsiblePerson) {
 		this.procedureResponsiblePerson = procedureResponsiblePerson;
 	}
+
 	public String getRegister() {
 		return register;
 	}
+
 	public void setRegister(String register) {
 		this.register = register;
 	}
+
 	public Date getRegisterTime() {
 		return registerTime;
 	}
+
 	public void setRegisterTime(Date registerTime) {
 		this.registerTime = registerTime;
 	}
+
 	public String getChecker() {
 		return checker;
 	}
+
 	public void setChecker(String checker) {
 		this.checker = checker;
 	}
-	public String getCheckTime() {
+
+	public Date getCheckTime() {
 		return checkTime;
 	}
-	public void setCheckTime(String checkTime) {
+
+	public void setCheckTime(Date checkTime) {
 		this.checkTime = checkTime;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "MProceduring [mPMId=" + mPMId + ", parentId=" + parentId + ", detailsNumber=" + detailsNumber
+				+ ", procedureId=" + procedureId + ", procedureName=" + procedureName + ", labourHourAmount="
+				+ labourHourAmount + ", costPrice=" + costPrice + ", subtotal=" + subtotal + ", procedureDescribe="
+				+ procedureDescribe + ", regCount=" + regCount + ", procedureResponsiblePerson="
+				+ procedureResponsiblePerson + ", register=" + register + ", registerTime=" + registerTime
+				+ ", checker=" + checker + ", checkTime=" + checkTime + "]";
+	}
 }
