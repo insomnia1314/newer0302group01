e3f760e67163950e47aca223337ebecdcd8383ff
GuangxiangLong <GuangxiangLong@GuangxiangLong-pc>
2016-10-26 18:34:57 +0800

    生产工序记录 物料记录 第一次上传

---
+++ softfactory/dao/com/softfactory/core/dao/MProcedureModulingMapper.java
@@ -0,0 +1,76 @@
+package com.softfactory.core.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+import com.softfactory.pojo.MProcedureModuling;
+
+/**
+ * 生产工序物料过程记录 DAO对象
+ * @author GuangxiangLong
+ *
+ */
+@Repository("mProcedureModulingMapper")
+public interface MProcedureModulingMapper {
+	
+	/**
+	 * 分页查询
+	 * @param page
+	 * @param pagesize
+	 * @param sort
+	 * @param order
+	 * @return
+	 */
+	List<MProcedureModuling> findPager(@Param("page")Integer page,@Param("pagesize")Integer pagesize,@Param("sort")String sort,@Param("order")String order);
+	
+	/**
+	 * 分页总记录数
+	 * @return
+	 */
+	long findPagerTotal();
+	
+	/**
+	 * 增加
+	 * @param m 持久化对象
+	 * @return 数据库影响记录数
+	 */
+	@Insert("INSERT INTO M_PROCEDURE_MODULING(ID,PARENT_ID,DETAILS_NUMBER,PRODUCT_ID,PRODUCT_NAME,COST_PRICE,AMOUNT,SUBTOTAL)"
+			+ "values(#{mPId},#{parentId},#{detailsNumber},#{producId},#{productName},#{costPrice},#{amount},#{subtotal})")
+	@SelectKey(statement="SELECT SEQ_M_PROCEDURE_MODULING.NEXTVAL FROM DUAL",before=true, keyProperty = "mPId", resultType =int.class)
+	int add(MProcedureModuling m);
+	
+	/**
+	 * 修改
+	 * @param m 持久化对象
+	 * @return 数据库影响记录数
+	 */
+	@Update("UPDATE M_PROCEDURE_MODULING SET PARENT_ID=#{parentId},DETAILS_NUMBER = #{detailsNumber},PRODUCT_ID=#{producId},PRODUCT_NAME=#{productName},COST_PRICE=#{costPrice},AMOUNT=#{amount},SUBTOTAL=#{subtotal} WHERE ID=#{mPId}")
+	int update(MProcedureModuling m);
+	
+	/**
+	 * 根据主键删除记录
+	 * @param mPId 需要匹配的主键
+	 * @return
+	 */
+	@Delete("delete from M_PROCEDURE_MODULING WHERE ID=#{mPId}")
+	int remove(Integer mPId);
+	
+	/**
+	 * 主键寻找单挑记录
+	 * @param mPId
+	 * @return
+	 */
+	@Select("select ID mPId,PARENT_ID parentId,DETAILS_NUMBER detailsNumber,PRODUCT_ID producId,PRODUCT_NAME productName,COST_PRICE costPrice,AMOUNT amount,SUBTOTAL subtotal"
+			+ " from M_PROCEDURE_MODULING where ID=#{mPId}")
+	MProcedureModuling findById(Integer mPId);
+	
+	
+
+}
---
+++ softfactory/dao/com/softfactory/core/dao/MProceduringMapper.java
@@ -0,0 +1,88 @@
+package com.softfactory.core.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+import com.softfactory.pojo.MProceduring;
+
+/**
+ * 生产工序记录
+ * 
+ * @author GuangxiangLong
+ *
+ */
+
+@Repository("mProceduringMapper")
+public interface MProceduringMapper {
+	
+	/**
+	 * 分页
+	 * @param page
+	 * @param pagesize
+	 * @param sort
+	 * @param order
+	 * @return
+	 */
+	List<MProceduring> findPager(@Param("page") Integer page, @Param("pagesize") Integer pagesize,
+			@Param("sort") String sort, @Param("order") String order);
+	
+	/**
+	 * 分页总记录数
+	 */
+	long findPagerTotal();
+		
+	/**
+	 * 增加方法
+	 * 
+	 * @param m
+	 * @return
+	 */
+	@Insert("INSERT INTO M_PROCEDURING(ID,PARENT_ID,DETAILS_NUMBER,PROCEDURE_ID,PROCEDURE_NAME,"
+			+ "LABOUR_HOUR_AMOUNT,COST_PRICE,SUBTOTAL,PROCEDURE_DESCRIBE,REG_COUNT,PROCEDURE_RESPONSIBLE_PERSON,"
+			+ "REGISTER,REGISTER_TIME,CHECKER,CHECK_TIME) "
+			+ "VALUES(#{mPMId},#{parentId},#{detailsNumber},#{procedureId},#{procedureName},"
+			+ "#{labourHourAmount},#{costPrice},#{subtotal},#{procedureDescribe},#{regCount},"
+			+ "#{procedureResponsiblePerson},#{register},#{registerTime},#{checker},#{checkTime})")
+	@SelectKey(before = true, keyProperty = "mPMId", resultType = int.class, statement = "SELECT SEQ_M_PROCEDURING.NEXTVAL FROM DUAL")
+	int add(MProceduring m);
+
+	/**
+	 * 修改方法
+	 * 
+	 * @param m
+	 * @return
+	 */
+	@Update("update M_PROCEDURING set PARENT_ID=#{parentId},DETAILS_NUMBER=#{detailsNumber},PROCEDURE_ID=#{procedureId},"
+			+ "PROCEDURE_NAME=#{procedureName},LABOUR_HOUR_AMOUNT=#{labourHourAmount},COST_PRICE=#{costPrice},SUBTOTAL=#{subtotal},"
+			+ "PROCEDURE_DESCRIBE=#{procedureDescribe},REG_COUNT=#{regCount},PROCEDURE_RESPONSIBLE_PERSON=#{procedureResponsiblePerson},"
+			+ "REGISTER=#{register},REGISTER_TIME=#{registerTime},CHECKER=#{checker},CHECK_TIME=#{checkTime} "
+			+ "WHERE ID=#{mPMId}")
+	int update(MProceduring m);
+	
+	/**
+	 * 通过主键寻找方法
+	 * @param mPMId
+	 * @return
+	 */
+	@Select("select ID mPMId, PARENT_ID parentId,DETAILS_NUMBER detailsNumber,PROCEDURE_ID procedureId,PROCEDURE_NAME ProcedureName,"
+			+ " LABOUR_HOUR_AMOUNT labourHourAmount,COST_PRICE costPrice,SUBTOTAL subtotal,PROCEDURE_DESCRIBE procedureDescribe,"
+			+ "  REG_COUNT regCount, PROCEDURE_RESPONSIBLE_PERSON procedureResponsiblePerson,REGISTER register,REGISTER_TIME registerTime,"
+			+ " CHECKER checker ,CHECK_TIME checkTime from M_PROCEDURING WHERE ID=#{mPMId}")
+	MProceduring findById(Integer mPMId);
+	
+	/**
+	 * 删除
+	 * @param mPMId
+	 * @return
+	 */
+	@Delete("DELETE FROM M_PROCEDURING WHERE ID=#{MPMID}")
+	int del(Integer mPMId);
+
+}
