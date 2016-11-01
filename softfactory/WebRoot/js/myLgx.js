bd36895d946e2480c018eea901c7fed7de66540d
GuangxiangLong <GuangxiangLong@GuangxiangLong-pc>
2016-11-01 15:58:12 +0800

    lgx

--- softfactory/WebRoot/js/myLgx.js
+++ softfactory/WebRoot/js/myLgx.js
@@ -1,7 +1,25 @@
 function eviewed(data) {
 	$("#divtabs").tabs("add", {
-		title : '测试',
+		title : '审核页面',
 		closable : true,
 		href : "MProceduring/showManufacture.do?id=1"
 	});
+}
+
+function changeFinishTag(data) {
+	$("#Myprocedure").window({
+		title : "生产记录",
+		width : 900,
+		height : 700,
+		modal : true,
+		minimizable : false,
+		href : "MProceduring/jumpChangeFinishTag.do?id="+data,
+//		onClose : function() {
+//			user_obj.search();
+//		}
+	});
+}
+
+function changeTransferTag() {
+	$("#Myprocedure").alert("this changeTransferTag");
 }
